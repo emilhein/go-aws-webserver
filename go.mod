24f949c351ba1989547de63a3b3914bff132cacf
Emilhein <emilhein1@gmail.com>
2019-10-27 10:20:06 +0200

    Updat edependecies + add a bit of comment

--- go.mod
+++ go.mod
@@ -4,6 +4,6 @@ go 1.13
 
 require (
 	github.com/aws/aws-sdk-go v1.25.17 // indirect
-	github.com/emilhein/go-aws-utility v1.0.2
+	github.com/emilhein/go-aws-utility v1.1.0
 	github.com/gorilla/mux v1.7.3
 )
--- webserver/webserver.go
+++ webserver/webserver.go
@@ -7,6 +7,8 @@ import (
 	"github.com/gorilla/mux"
 )
 
+const portNumber = 3001
+
 func Start() {
 	r := mux.NewRouter()
 
@@ -14,6 +16,7 @@ func Start() {
 	r.HandleFunc("/getS3files", GetS3Files).Methods("POST")
 	r.HandleFunc("/interfaces", InterfaceMethod)
 	r.HandleFunc("/startmining", StartMining)
+	fmt.Printf("Server started on port %v \n", portNumber)
 	err := http.ListenAndServe(":3001", r)
 	if err != nil {
 		fmt.Printf("Could not start the server: %v", err)
